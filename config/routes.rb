13cf04bd9381898d8a03b6cadf8b1014d4ed7d17
takuya828 <takuya11070221@gmail.com>
2021-09-15 08:10:05 +0000

    [Add] 管理者ログイン画面、ジャンル一覧画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+     resources :genres, only: [:new, :index, :create, :edit, :update]
+  end
+
 end
