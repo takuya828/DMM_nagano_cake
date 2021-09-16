d32b1696f88c366db55da38c7e68b32121194e17
takuya828 <takuya11070221@gmail.com>
2021-09-16 15:19:00 +0000

    [Add]管理者側の商品新規登録　ジャンルプルダウンを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_130433) do
+ActiveRecord::Schema.define(version: 2021_09_16_144325) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_09_15_130433) do
     t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre_id"
   end
 
 end
