5bac8fed8b62b90b31f1a2da066f8db5fd187a6b
takuya828 <takuya11070221@gmail.com>
2021-09-17 09:30:33 +0000

    [Add]商品一覧ページ、会員一覧ページ作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,16 +1,21 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
+    @items = Item.all
   end
-  
+
   def new
     @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to new_admin_item_path
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
@@ -18,5 +23,10 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
-  
+
+   private
+  def item_params
+    params.require(:item).permit(:id, :genre_id, :name, :image_id, :introduction, :price)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :customers
   devise_for :admins
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
      resources :genres, only: [:new, :index, :create, :edit, :update]
      resources :items
+     resources :customers
   end
 
 end
