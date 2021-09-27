38f72a1293fd2a301db7ddb2e0386c42b96a60be
takuya828 <takuya11070221@gmail.com>
2021-09-27 13:16:28 +0000

    [Fix]注文情報確認ページの修正、商品や支払い方法の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,22 @@ class OrdersController < ApplicationController
   def index
   end
 
+  def check
+    @order = Order.new(order_params)
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+    @address = Address.find(params[:order][:address_id])
+    @cart_items = CartItem.all
+    @cart_item = CartItem
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+    @postage = 800
+  end
+
   def new
     @customer = current_customer
     @addresses = current_customer.addresses
+    @address = Address.new
     @order = Order.new
   end
 
